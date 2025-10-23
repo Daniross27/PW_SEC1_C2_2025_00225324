89869eb403b3eadad1f23c83fee56ee9282ca611
Daniross27 <00225324@uca.edu.sv>
2025-10-23 00:20:44 -0600

    feat:final

--- src/routes/cuentas.routes.js
+++ src/routes/cuentas.routes.js
@@ -1,16 +1,18 @@
 import express from "express";
 import {
-getCuentas,
-getCuentaById,
-getCuentaByParam,
-getBalanceActivo
+    getCuentas,
+    getCuentaById,
+    getCuentaByParam,
+
+    getBalanceActivo 
 } from "../controllers/cuentas.controller.js";
 
 const router = express.Router();
 
 router.get("/cuentas", getCuentas);
 router.get("/cuenta/:id", getCuentaById);
-router.get("/cuentasBalance", getTotalBalance);
+
+router.get("/cuentasBalance", getBalanceActivo); 
 router.get("/buscar", getCuentaByParam);
 
 export default router;
